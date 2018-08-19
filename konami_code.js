b4d6a258dde7883cedfde91cb392a85859225672
min kim <minwookkim2013@u.northwestern.edu>
2018-08-19 18:01:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,18 @@ const codes = [
   "b",
   "a"
 ];
-
+const key = [38,38,40,40,37,37,39,39,66,65]
 function init() {
-  // your code here
+  let index = 0;
+  body.document.addEventListener('keydown', function(e){
+    if (e.which === key[index]){
+      index ++;
+    if (index === 9){
+      alert("Please Help!");
+      index = 0;
+    }
+  } else {
+    index =0;
+  }
+})
 }
