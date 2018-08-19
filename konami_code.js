f7a2fff956adefa659e5945a8c85e9baeeb33ed0
min kim <minwookkim2013@u.northwestern.edu>
2018-08-19 18:54:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const key = [38,38,40,40,37,37,39,39,66,65]
 function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(e){
-    if (e.which === key[index]){
+    if (e.key === key[index]){
       index++;
   } else {
     index =0;
